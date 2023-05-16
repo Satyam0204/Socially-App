c131ef3fb0489d9ad4a6c2afa4cb9c59de83962f
Satyam0204 <satyampattanaik007@gmail.com>
2023-05-16 12:52:02 +0530

    [ADD] inital code and file structure

---
+++ sociallyApp/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  )
+}
+
+
+
+export default App;
