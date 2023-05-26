525d0197859227ce186d4ddaec382ab2ce2bd3d5
Satyam0204 <satyampattanaik007@gmail.com>
2023-05-26 16:15:00 +0530

    [FEATURE] user data stored in app data

--- sociallyApp/src/context/AuthContext.jsx
+++ sociallyApp/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React,{useState,useEffect, createContext} from 'react'
 import jwtDecode from "jwt-decode";
 import {BASE_URL} from "@env"
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AuthContext=createContext()
 
@@ -10,14 +10,18 @@ const AuthContext=createContext()
 export default AuthContext;
 
 export const AuthProvider=({children})=>{
-    let [user,setUser]=useState(null)
-    let [token,settoken]=useState(null)
+    // const [token, settoken] = useState(null);
+    // const [user, setUser] = useState(null);
+    let [token,settoken]=useState(async ()=> await AsyncStorage.getItem('token')?await JSON.stringify(AsyncStorage.getItem('token')):null)
+    let [user,setUser]=useState(async ()=>await AsyncStorage.getItem('token')?await jwtDecode(AsyncStorage.getItem('token')):null)
+
 
     let loginuser=async (username,password)=>{
         
 
-        console.log(BASE_URL)
+        // console.log(BASE_URL)
         try {
+            AsyncStorage.clear()
             // console.log("username: "+username+" password: "+password)
             let response=await fetch(`${BASE_URL}/api/login`,{
                 method:'POST',
@@ -28,10 +32,15 @@ export const AuthProvider=({children})=>{
             })
             let data = await response.json()
             if(response.status===200){
-
-                settoken(data)
+                
+                settoken(data.access)
                 setUser(jwtDecode(data.access))
+                
+                await AsyncStorage.setItem('token',JSON.stringify(data.access))
 
+            }
+            else{
+                console.error(data)
             }
         } catch (error) {
             console.log("error: "+error)
