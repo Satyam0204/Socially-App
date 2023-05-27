816a5366a1817bdede14c08b4bcbf8dba9e19827
Satyam0204 <satyampattanaik007@gmail.com>
2023-05-27 22:23:59 +0530

    [UPDATE] fetching posts from backend

--- sociallyApp/src/components/posts/PostItemList.jsx
+++ sociallyApp/src/components/posts/PostItemList.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import {BASE_URL} from "@env"
+import React,{useState,useEffect,useContext} from "react"
+import AuthContext from "../../context/AuthContext";
 import {View, Text,Image,StyleSheet} from 'react-native';
 import Posts from '../../db/db';
 import Postitem from './Postitem';
 
+
 const PostItemList = () => {
+    let {token}=useContext(AuthContext)
+
+    const [posts, setPosts] = useState([])
+    let apiheaders={
+        'Content-type':"application/json",
+        'Authorization':'Bearer '+token
+    }
+    const getPosts = async()=>{
+        console.log(token)
+        console.log("hello")
+        let response=await fetch(BASE_URL+'/api/all_users_posts/',{
+            method:'GET',
+            headers:apiheaders
+    })
+        let data = await response.json()
+        console.log(data)
+        setPosts(data)
+
+    }
+    useEffect(() => {
+        getPosts()
+        
+    }, []);
+    
+  
     return (
         <View>
             {Posts.map((post)=>(    
