56168c5aa9be78627c732b01c6b6272b38d02051
Satyam0204 <satyampattanaik007@gmail.com>
2023-05-29 13:36:12 +0530

    [UPDATE] fetching profile followers and following count from backend

--- sociallyApp/src/screens/ProfileScreen.jsx
+++ sociallyApp/src/screens/ProfileScreen.jsx
@@ -1,20 +1,46 @@
-import React,{useContext,useEffect} from 'react';
+import React,{useState,useContext,useEffect} from 'react';
+import {BASE_URL} from '@env'
 import {View,Text, StyleSheet, TouchableOpacity} from 'react-native';
 import AuthContext from '../context/AuthContext';
 
 const ProfileScreen = ({navigation}) => {
-    let {user}=useContext(AuthContext)
+    let {user,token}=useContext(AuthContext)
     let{logoutUser}=useContext(AuthContext)
+
+    const [followers, setfollowers] = useState();
+    const [following, setfollowing] = useState();
+
+    let getProfile=async ()=>{
+        let response= await fetch(`${BASE_URL}/api/user`,{
+            method:"GET",
+            headers:{
+                'Content-Type':'application/json',
+                "Authorization":'Bearer '+token
+            }
+        })  
+        
+        let data = await response.json()
+        setfollowers(data.followers)
+        setfollowing(data.following)
+    }
+
 
     useEffect(() => {
         if(user==null){
             navigation.navigate('login')
         }
+        getProfile()
         
     },[user]);
     return (
         <View>
-            <Text style={{color:"black"}}>This is Profile Screen for {user?user.username:""}</Text>
+            <Text style={{color:"black"}}>{user?user.username:""}</Text>
+
+        <View style={styles.stats}>
+            <Text style={styles.statsText}>Followers: {followers}</Text>
+            <Text style={styles.statsText}>Following: {following}</Text>
+        </View>
+
             <TouchableOpacity
             onPress={()=>{
                 logoutUser()
@@ -37,5 +63,10 @@ const ProfileScreen = ({navigation}) => {
 }
 
 
+const styles= StyleSheet.create({
+    statsText:{
+        color:"black"
+    }
+})
 
 export default ProfileScreen;
