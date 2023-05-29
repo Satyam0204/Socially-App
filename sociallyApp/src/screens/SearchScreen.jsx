43a0e0942da541903b56eca93f499a9202f590c6
Satyam0204 <satyampattanaik007@gmail.com>
2023-05-30 00:38:26 +0530

    [UPDATE] separated the search result component and added route params

--- sociallyApp/src/screens/SearchScreen.jsx
+++ sociallyApp/src/screens/SearchScreen.jsx
@@ -1,80 +1,76 @@
-import {BASE_URL} from '@env'
+import {BASE_URL} from '@env';
 
-import React,{useState,useEffect} from 'react';
-import {View, Text,StyleSheet} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import SearchResult from '../components/SearchResult';
 
-const SearchScreen = () => {
-    var [searchbar,setsearchbar]=useState("")
+const SearchScreen = ({navigation}) => {
+  var [searchbar, setsearchbar] = useState();
 
-    let [results, setResults]=useState([])
+  let [results, setResults] = useState([]);
 
+  let searchProfile = async () => {
+    let response = await fetch(`${BASE_URL}/api/search`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({profile: searchbar}),
+    });
+    let data = await response.json();
 
-   
-      let searchProfile = async () =>{
+    setResults(data);
+  };
 
-        let response = await fetch(`${BASE_URL}/api/search`,{
-          method:"POST",
-          headers:{
-            "Content-Type":"application/json"
-          },
-          body:JSON.stringify({"profile":searchbar})
-        })
-        let data = await response.json() 
+  useEffect(() => {
+    if (searchbar) {
+      searchProfile();
+    } else {
+      setResults([]);
+    }
+  }, [searchbar]);
 
-        setResults(data)
-      }
-    
+  return (
+    <View>
+      <Text style={{color: 'black'}}>this is the search screen</Text>
 
-
- useEffect(() => {
-  if(searchbar){
-    searchProfile()
-}
-
-  else{
-    setResults([])
-  }
- }, [searchbar]);
-
-    return (
-        <View>
-            <Text style={{color:"black"}}>
-                this is the search screen
-            </Text>
-
-
-            <View><TextInput placeholder='search here' placeholderTextColor="black" clearButtonMode="always" style={styles.inputfield} value={searchbar}
-                        onChangeText={(searchbar) => {
-                            setsearchbar(searchbar)
-                        }}/></View>
-
-        <View>
-        {results.map((result,index)=>(
-          <View key={index}>
-             <Text style={styles.results} >
-             {result.username}
-
-             </Text>
-            </View>
-
-        ))}
+      <View>
+        <TextInput
+          placeholder="search here"
+          placeholderTextColor="black"
+          clearButtonMode="always"
+          style={styles.inputfield}
+          value={searchbar}
+          onChangeText={searchbar => {
+            setsearchbar(searchbar);
+          }}
+        />
       </View>
 
+      {results.map((result, index) => (
+        <View key={index}>
+          <SearchResult result={result} navigation={navigation}/>
         </View>
-    );
-}
+      ))}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    inputfield:{
-        color:"black", 
-        borderWidth:1,
-        margin:5,
-        padding:5
-    },
-    results:{
-        color:"black"
-    }
-})
+  inputfield: {
+    color: 'black',
+    borderWidth: 1,
+    margin: 5,
+    padding: 5,
+  },
+  results: {
+    color: 'black',
+    borderWidth: 2,
+    borderColor: 'black',
+    margin: 15,
+    padding: 2,
+  },
+});
 
 export default SearchScreen;
