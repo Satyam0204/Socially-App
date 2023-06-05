e524b8caae5f7b712dfd12d83b6e7d5e8434de4e
Satyam0204 <satyampattanaik007@gmail.com>
2023-06-05 17:48:28 +0530

    [UPDATE] styling the stats container

--- sociallyApp/src/screens/ProfileScreen.jsx
+++ sociallyApp/src/screens/ProfileScreen.jsx
@@ -37,7 +37,7 @@ const ProfileScreen = ({navigation}) => {
     },);
     return (
         <View>
-            <ImageBackground resizeMode="cover" style={styles.cover} source={{uri:"https://img.freepik.com/free-photo/shallow-focus-shot-young-attractive-male-posing-camera_181624-42193.jpg?w=740&t=st=1685709507~exp=1685710107~hmac=12453ec329112d6e965c48dc84c402314685858578331997a42484c51809900d"}}>
+            <ImageBackground  resizeMode="cover" style={styles.cover} source={{uri:"https://img.freepik.com/free-photo/shallow-focus-shot-young-attractive-male-posing-camera_181624-42193.jpg?w=740&t=st=1685709507~exp=1685710107~hmac=12453ec329112d6e965c48dc84c402314685858578331997a42484c51809900d"}}>
           
         <View style={styles.stats}>
             <Text style={styles.statsText}>{user?user.username:""}</Text>
@@ -77,6 +77,13 @@ const styles= StyleSheet.create({
         
     },
     stats:{
+        position:"absolute",
+        bottom:50,
+        width:327,
+        margin:20,
+        backgroundColor: 'rgba(52, 52, 52, 0.6)',
+        height:300,
+
 
     }
 })
